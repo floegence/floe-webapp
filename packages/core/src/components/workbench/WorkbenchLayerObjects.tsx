ccdfe8cb7c24f80d7535802cfb8229ea07c801cd
jianyintang <tangjianyin@gmail.com>
2026-05-18 23:52:45 +0800

    fix(workbench): use boolean true for contentEditable to satisfy TS type

--- packages/core/src/components/workbench/WorkbenchLayerObjects.tsx
+++ packages/core/src/components/workbench/WorkbenchLayerObjects.tsx
@@ -804,7 +804,7 @@ export function WorkbenchStickyNote(props: {
         <div
           ref={bodyEditor.bind}
           class="workbench-sticky__body"
-          contentEditable={locked() ? false : 'true'}
+          contentEditable={locked() ? false : true}
           role="textbox"
           aria-multiline="true"
           aria-disabled={locked() ? 'true' : undefined}
